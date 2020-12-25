f0b519335379efe6bf3e8cb6a7417bde4cb79ddb
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-25 09:30:41 +0800

    输出材质

--- src/4.2 Stencil testing/stencil_testing.cpp
+++ src/4.2 Stencil testing/stencil_testing.cpp
@@ -73,8 +73,8 @@ int main()
 
     // build and compile shaders
     // -------------------------
-    char *vsPath = "/src/4.1 Depth Testing/depth_testing.vs";
-    char *fsPath = "/src/4.1 Depth Testing/depth_testing.fs";
+    char *vsPath = "/src/4.2 Stencil testing/stencil_testing.vs";
+    char *fsPath = "/src/4.2 Stencil testing/stencil_testing.fs";
     ShaderLoader shader(vsPath, fsPath, nullptr);
 
     // set up vertex data (and buffer(s)) and configure vertex attributes

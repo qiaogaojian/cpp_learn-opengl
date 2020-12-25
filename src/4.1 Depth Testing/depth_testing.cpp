ea9902d4c2d5cba971bb3cbd1e33f2c486d85fd3
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-25 09:40:09 +0800

    解决深度冲突

--- src/4.1 Depth Testing/depth_testing.cpp
+++ src/4.1 Depth Testing/depth_testing.cpp
@@ -125,13 +125,13 @@ int main()
     };
     float planeVertices[] = {
         // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
-         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
-        -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
-        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+         5.0f, -0.51f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.51f,  5.0f,  0.0f, 0.0f,
+        -5.0f, -0.51f, -5.0f,  0.0f, 2.0f,
 
-         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
-        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
-         5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+         5.0f, -0.51f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.51f, -5.0f,  0.0f, 2.0f,
+         5.0f, -0.51f, -5.0f,  2.0f, 2.0f
     };
     // cube VAO
     unsigned int cubeVAO, cubeVBO;

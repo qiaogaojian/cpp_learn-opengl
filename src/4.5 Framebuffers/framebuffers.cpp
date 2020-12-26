fcec0c0814accf6a08c1ff5826b63cc58feb4be2
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-26 11:50:35 +0800

    我们可以将屏幕纹理的环绕方式都设置为GL_CLAMP_TO_EDGE。这样子在取到纹理外的像素时，就能够重复边缘的像素来更精确地估计最终的值了

--- src/4.5 Framebuffers/framebuffers.cpp
+++ src/4.5 Framebuffers/framebuffers.cpp
@@ -195,6 +195,8 @@ int main()
     glGenTextures(1, &texScreen);
     glBindTexture(GL_TEXTURE_2D, texScreen);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glBindTexture(GL_TEXTURE_2D, 0);

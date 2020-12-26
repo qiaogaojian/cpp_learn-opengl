54509153aeebd9a0ed7b6ed81fa499df68b98dd6
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-26 11:24:42 +0800

    comment

--- src/4.5 Framebuffers/framebuffers_screen.fs
+++ src/4.5 Framebuffers/framebuffers_screen.fs
@@ -9,7 +9,7 @@ void main()
 {
     FragColor = texture(screenTexture, TexCoords);
     // float average = (FragColor.r + FragColor.g + FragColor.b)/3; // 黑白
-    人眼会对绿色更加敏感一些，而对蓝色不那么敏感，所以为了获取物理上更精确的效果，我们需要使用加权的(Weighted)通道
+    // 人眼会对绿色更加敏感一些，而对蓝色不那么敏感，所以为了获取物理上更精确的效果，我们需要使用加权的(Weighted)通道
     float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
     FragColor = vec4(vec3(average), 1.0);
     // FragColor = vec4(1 - texture(screenTexture, TexCoords).rgb, 1.0); // 反相

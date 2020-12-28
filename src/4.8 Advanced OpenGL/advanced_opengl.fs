631675ba2611fa9aa5dd9861affd62e28c1937a9
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-28 14:25:16 +0800

    测试 gl_FrontFacing

--- src/4.8 Advanced OpenGL/advanced_opengl.fs
+++ src/4.8 Advanced OpenGL/advanced_opengl.fs
@@ -10,10 +10,15 @@ uniform float alpha;
 
 void main()
 {
-	// FragColor = vec4(1.0f,0.5f,0.2f,1.0f);
-    if (gl_FragCoord.x < 400){
-	    FragColor = texture(texture1, TexCoord);
-    } else {
-	    FragColor = texture(texture2, vec2(-TexCoord.x,TexCoord.y));
+    // FragColor = vec4(1.0f,0.5f,0.2f,1.0f);
+    // if (gl_FragCoord.x < 400){
+        //     FragColor = texture(texture1, TexCoord);
+    // } else {
+        //     FragColor = texture(texture2, vec2(-TexCoord.x,TexCoord.y));
+    // }
+    if(gl_FrontFacing){
+        FragColor=texture(texture1,TexCoord);
+    }else{
+        FragColor=texture(texture2,vec2(-TexCoord.x,TexCoord.y));
     }
 }

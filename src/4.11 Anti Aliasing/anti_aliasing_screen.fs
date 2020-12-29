89d752ebcff0faeeb8c472d4651c04a2a08f8a87
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-29 16:42:11 +0800

    使用高斯模糊

--- src/4.11 Anti Aliasing/anti_aliasing_screen.fs
+++ src/4.11 Anti Aliasing/anti_aliasing_screen.fs
@@ -9,36 +9,36 @@ const float offset = 1.0/300.0;
 
 void main()
 {
-    //   vec2 offsets[9] = vec2[](
-    //     vec2(-offset,  offset), // 左上
-    //     vec2( 0.0f,    offset), // 正上
-    //     vec2( offset,  offset), // 右上
-    //     vec2(-offset,  0.0f),   // 左
-    //     vec2( 0.0f,    0.0f),   // 中
-    //     vec2( offset,  0.0f),   // 右
-    //     vec2(-offset, -offset), // 左下
-    //     vec2( 0.0f,   -offset), // 正下
-    //     vec2( offset, -offset)  // 右下
-    // );
+      vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // 左上
+        vec2( 0.0f,    offset), // 正上
+        vec2( offset,  offset), // 右上
+        vec2(-offset,  0.0f),   // 左
+        vec2( 0.0f,    0.0f),   // 中
+        vec2( offset,  0.0f),   // 右
+        vec2(-offset, -offset), // 左下
+        vec2( 0.0f,   -offset), // 正下
+        vec2( offset, -offset)  // 右下
+    );
 
-    // float kernel[9] = float[](
-    //     // 锐化卷积矩阵 周围的像素和中心越相似 中心值越小 反差越大 中心值越大
-    //     -1, -1, -1,
-    //     -1,  9, -1,
-    //     -1, -1, -1
-    // );
+    float kernel[9] = float[](
+        // 模糊卷积矩阵2高斯模糊 平均中心和周围像素
+        1.0 / 16, 2.0 / 16, 1.0 / 16,
+        2.0 / 16, 4.0 / 16, 2.0 / 16,
+        1.0 / 16, 2.0 / 16, 1.0 / 16
+    );
 
-    // vec3 sampleTex[9];
-    // for(int i = 0; i < 9; i++){
-    //     sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
-    // }
-    // vec3 col = vec3(0.0);
-    // for(int i = 0; i < 9; i++){
-    //     col += sampleTex[i] * kernel[i];
-    // }
-    // FragColor = vec4(col, 1.0);
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++){
+        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+    }
+    vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++){
+        col += sampleTex[i] * kernel[i];
+    }
+    FragColor = vec4(col, 1.0);
 
-    FragColor = texture(screenTexture, TexCoords);
+    // FragColor = texture(screenTexture, TexCoords);
     // float average = (FragColor.r + FragColor.g + FragColor.b)/3; // 黑白
     // 人眼会对绿色更加敏感一些，而对蓝色不那么敏感，所以为了获取物理上更精确的效果，我们需要使用加权的(Weighted)通道
     // float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;

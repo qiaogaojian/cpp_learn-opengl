bdaf3f10f71dd90b76062175b045c83ec11070ee
qiaogaojian <qiaogaojian@vip.qq.com>
2021-01-08 15:55:05 +0800

    编写text渲染方法

--- src/7.2 Text Rendering/text_rendering.cpp
+++ src/7.2 Text Rendering/text_rendering.cpp
@@ -1,7 +1,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-
+#include "tools.h"
+#include <map>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "direct.h"
+#include "shader_loader.h"
 #include "ft2build.h"
 #include FT_FREETYPE_H
 
@@ -33,6 +39,17 @@ float vertices[] = {
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+void RenderText(ShaderLoader &s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color);
+
+struct Character{
+    unsigned int TextureID; // 字形纹理的ID
+    vec2 Size;              // 字形大小
+    vec2 Bearing;           // 从基准线到左部/顶部的偏移值
+    unsigned int Advance;   // 原点到下一个字形的距离
+};
+
+map<char, Character> characters;
+GLuint textVAO,textVBO;
 
 int main()
 {
@@ -137,16 +154,67 @@ int main()
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
+    char *vsPath = "/src/7.2 Text Rendering/text_rendering.vert";
+    char *fsPath = "/src/7.2 Text Rendering/text_rendering.frag";
+    ShaderLoader shader(vsPath, fsPath, nullptr);     // 受光物体shader程序
+
     FT_Library ft;
     if (FT_Init_FreeType(&ft))
     {
         cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
     }
     FT_Face face;
-    if (FT_New_Face(ft, "res/font/msyh.ttf", 0, &face))
+    string facePath = concatString(getcwd(NULL, 0), "/res/font/msyh.ttc");
+    if (FT_New_Face(ft, facePath.c_str(), 0, &face))
     {
         std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
     }
+    FT_Set_Pixel_Sizes(face, 0, 48);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    for (GLubyte c = 0; c < 128; c++)
+    {
+        if (FT_Load_Char(face,c,FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RED,face->glyph->bitmap.width,face->glyph->bitmap.rows,0,GL_RED,GL_UNSIGNED_BYTE,face->glyph->bitmap.buffer);
+
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
+        Character character = {
+            texture,
+            vec2(face->glyph->bitmap.width,face->glyph->bitmap.rows),
+            vec2(face->glyph->bitmap_left,face->glyph->bitmap_top),
+            face->glyph->advance.x
+        };
+        characters.insert(pair<char,Character>(c,character));
+    }
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    mat4 projection = ortho(0, 800, 0, 600);
+
+
+    glGenVertexArrays(1, &textVAO);
+    glGenBuffers(1, &textVBO);
+    glBindVertexArray(textVAO);
+    glBindBuffer(GL_ARRAY_BUFFER,textVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*6*4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,4,GL_FLOAT,GL_FALSE,4*sizeof(GLfloat),0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -162,6 +230,8 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
 
+        // RenderText(shader,"Hello Triangle",400,300,1,toFloatColor("FFFFFF"));
+        RenderText(shader, "(C) LearnOpenGL.com", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
         // 检查并调用事件，交换缓冲完成绘制
         glfwPollEvents();        // 检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）
         glfwSwapBuffers(window); // 双缓冲交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上
@@ -188,4 +258,48 @@ void processInput(GLFWwindow *window)
     {
         glfwSetWindowShouldClose(window, true);
     }
+}
+
+void RenderText(ShaderLoader &s, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
+{
+    // 激活对应的渲染状态
+    s.use();
+    glUniform3f(glGetUniformLocation(s.ID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(textVAO);
+
+    // 遍历文本中所有的字符
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = characters[*c];
+
+        GLfloat xpos = x + ch.Bearing.x * scale;
+        GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        GLfloat w = ch.Size.x * scale;
+        GLfloat h = ch.Size.y * scale;
+        // 对每个字符更新VBO
+        GLfloat vertices[6][4] = {
+            { xpos,     ypos + h,   0.0, 0.0 },
+            { xpos,     ypos,       0.0, 1.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+
+            { xpos,     ypos + h,   0.0, 0.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+            { xpos + w, ypos + h,   1.0, 0.0 }
+        };
+        // 在四边形上绘制字形纹理
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // 更新VBO内存的内容
+        glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // 绘制四边形
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // 更新位置到下一个字形的原点，注意单位是1/64像素
+        x += (ch.Advance >> 6) * scale; // 位偏移6个单位来获取单位为像素的值 (2^6 = 64)
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
